953bd0bb395655c01ea17cc1a1db28b6cc20ae74
ranfyy <ranfyy@gmail.com>
2018-02-03 14:50:23 +0100

    Update textmono.php

--- server/contents/textmono.php
+++ server/contents/textmono.php
@@ -3,7 +3,7 @@ $fontSize = $scale;
 $cursorY = 0;
 $cursorY += $fontSize*1.5;
 $fontSize = 0.5*$scale;
-$arr = file( "w" );
+$arr = file( "sample.txt" );
 foreach( $arr as $line ) {
         imagettftext($im, $fontSize, 0, 20, $cursorY, $black, $DEFAULT_FONT['notomono'], $line );
         $cursorY += $fontSize*1.5;
