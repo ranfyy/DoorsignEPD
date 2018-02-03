b290e0241c799e284ffff25d3fb02fbfde460b4b
ranfyy <ranfyy@gmail.com>
2018-02-03 14:32:03 +0100

    Create textmono.php

---
+++ server/contents/textmono.php
@@ -0,0 +1,13 @@
+<?php
+
+$fontSize = $scale;
+
+$cursorY = 0;
+$cursorY += $fontSize*1.5;
+$fontSize = 0.5*$scale;
+
+$arr = file( "w" );
+foreach( $arr as $line ) {
+        imagettftext($im, $fontSize, 0, 20, $cursorY, $black, $DEFAULT_FONT['notomono'], $line );
+        $cursorY += $fontSize*1.5;
+}
