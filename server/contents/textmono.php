6ed46c980058cf1920d6c29b1eeb1f4047d9f791
ranfyy <ranfyy@gmail.com>
2018-02-11 09:47:30 +0100

    Update textmono.php

--- server/contents/textmono.php
+++ server/contents/textmono.php
@@ -1,8 +1,24 @@
 <?php
+#define( "DEBUG", true );
+define( "DEBUG", false );
+
 $fontSize = 0.5 *$scale;
 $cursorY  = $fontSize *1.5;
+$green = imagecolorallocate($im, 0, 255, 0);
+$blue  = imagecolorallocate($im, 0, 0, 255);
+
+$dst_x = $displayWidth  - $cursorY;
+$dst_y = $displayHeight - $cursorY;
 $arr = file( "sample.txt" );
 foreach( $arr as $line ) {
-    imagettftext($im, $fontSize, 0, 20, $cursorY, $black, $DEFAULT_FONT['notomono'], $line );
-    $cursorY += $fontSize *1.5;
+    imagettftext($im, $fontSize, 0, $fontSize*1.5, $cursorY, $black, $DEFAULT_FONT['notomono'], $line );
+    $cursorY += $fontSize*1.5;
 }
+
+// right side
+$bg = $background_color;
+if( DEBUG ) $bg = $blue;
+imagefilledrectangle( $im, $dst_x, 0, $cursorY, $displayHeight, $bg );
+// bottom
+if( DEBUG ) $bg = $green;
+imagefilledrectangle( $im, 0, $dst_y, $displayWidth, $cursorY, $bg );
