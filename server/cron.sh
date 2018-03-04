11f6246327b4098eb56422b073496f4fcab9e4f2
ranfyy <ranfyy@gmail.com>
2018-03-04 14:59:37 +0100

    darksky weather forecast -> DoorsignEPD -> epd2.7 -> display

--- server/cron.sh
+++ server/cron.sh
@@ -1,1 +1,16 @@
-top -b -n 1 -c > sample.txt
+DIRTO="~/src/DoorsignEPD/server/"
+pushd ~/src/darksky/
+    php index.php
+    cp sample.txt $DIRTO
+popd
+
+pushd $DIRTO
+    # top -b -n 1 -c > sample.txt
+    php index.php
+    DIRTO="~/src/raspberrypi/python/"
+    cp sample.png $DIRTO
+popd
+
+pushd $DIRTO
+    sudo python test.main.py
+popd
